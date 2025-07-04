545ba24c39e70677b2931f35706ce594981fabbb
Nguyễn Văn Diễn <66367482+v3r1f4@users.noreply.github.com>
2025-07-04 17:41:39 +0700

    first commit

---
+++ camera_calibration.py
@@ -0,0 +1,60 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(8,5,0)
+# For 7x10 squares, we have 6x9 internal corners
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob('samples/*.jpg')
+
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (9,6), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (9,6), corners2, ret)
+        #cv.imshow('img', img)
+        #cv.waitKey(500)
+
+cv.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+np.savez('calib_data.npz', mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+
+img1 = cv.imread('samples/1.jpg')
+h1, w1 = img1.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w1,h1), 1, (w1,h1))
+
+dst = cv.undistort(img1, mtx, dist, None, newcameramtx)
+
+x, y, w1, h1 = roi
+dst = dst[y:y+h1, x:x+w1]
+cv.imwrite('result1.jpg', dst)
+
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
+ 
+print( "total error: {}".format(mean_error/len(objpoints)) )
---
+++ preprocessing.py
@@ -0,0 +1,5 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+img = glob.glob('samples/ArUco.jpg')
